5f519f49e2592293439d9b80d4d181ef6c639995
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-08 16:03:13 +0545

    django setup

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',  # Added 'myapp' to the list of installed apps
+    # Added 'myapp' to the list of installed apps
 ]
 
 MIDDLEWARE = [
