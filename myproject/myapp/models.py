cc1fa9e29c54a8feff648718548583b31b037348
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-08 19:08:27 +0545

    backend connected

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Contact(models.Model):
     name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
     email = models.EmailField()
+    phone = models.CharField(max_length=15)  # Add the phone field here
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
