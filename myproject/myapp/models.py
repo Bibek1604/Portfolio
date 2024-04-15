12fcc87f8202d8d4639284e7207acc36c577c3a0
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-15 13:06:44 +0545

    table added

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+    name = models.CharField(max_length=100)##NAME FIELD HERE
+    email = models.EmailField()#EMAIL FILD HERE
     phone = models.CharField(max_length=15)  # Add the phone field here
-    message = models.TextField()
+    message = models.TextField() #MESSAGE FIELD HERE
 
     def __str__(self):
         return self.name
