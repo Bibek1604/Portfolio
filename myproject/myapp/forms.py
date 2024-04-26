ffe67a7e9b643d2c01708aebcd0b0ad18bc6fce8
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-26 22:53:16 +0545

    deletedd all the file

--- myproject/myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Contact  # Import the Contact model from the same directory
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ['name', 'phone', 'email']
--- myproject/myproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from myapp.views import bibek, contact
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Include the admin URLs
-    path('', bibek, name='bibek'),
-    # path('contact/', contact, name='contact'),
-]
